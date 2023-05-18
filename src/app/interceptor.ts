18ab6e08004910ccc4557152cdc38bd4abd52167
Warczachowski <wlodek_w@yahoo.de>
2023-05-18 14:06:01 +0200

    remove api ;)

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = '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';
-   // const myHeaders = new Headers();
-   // myHeaders.set('Authorization: Bearer ', token);
+    const token = environment.api_key;
+
      const authReq = req.clone({
       headers: req.headers.set('Authorization', 'Bearer '+token)
-      //.set('Content-Type', 'text/html')
+
     });
 
     return next.handle(authReq);
